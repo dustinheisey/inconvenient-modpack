fe23bc4965f413674eee46d18bf1472ff526f337
Dustin Heisey <dustin@heisey.email>
2024-03-09 12:00:20 +0100

    fix: show create compounds

--- kubejs/client_scripts/show_items.js
+++ kubejs/client_scripts/show_items.js
@@ -3,6 +3,11 @@ onEvent('jei.add.items', event => {
     realistictorches: ['realistictorches:unlit_torch'],
     endrem: ['endrem:cryptic_eye'],
     cataclysm: ['cataclysm:witherite_ingot'],
+    create: [
+      'create:chromatic_compound',
+      'create:refined_radiance',
+      'create:shadow_steel',
+    ],
   }).forEach(item => {
     event.add(item);
   });
