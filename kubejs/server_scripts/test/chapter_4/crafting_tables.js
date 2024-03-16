9e78c3e8219942b395676fec9cae8fe689d3ab84
Dustin Heisey <dustin@heisey.email>
2024-03-16 10:15:15 +0100

    refactor: start crafting_tables recipe idea

---
+++ kubejs/server_scripts/test/chapter_4/crafting_tables.js
@@ -0,0 +1,16 @@
+onEvent('recipes', event => {
+  let crafting_tables = [
+    {id: 'spout', recipe: () => {}},
+    {id: 'mechanical_press', recipe: () => {}},
+    {id: 'basin', recipe: () => {}},
+    {id: 'foundry', recipe: () => {}},
+    {id: 'encased_fan', recipe: () => {}},
+    {id: 'mechanical_mixer', recipe: () => {}},
+    {id: 'crushing_wheel', recipe: () => {}},
+    {id: 'blaze_burner', recipe: () => {}},
+    {id: 'evaporation_tower', recipe: () => {}},
+    {id: 'drain', recipe: () => {}},
+    {id: 'tesla_coil', recipe: () => {}},
+    {id: 'alternator', recipe: () => {}},
+  ];
+});
